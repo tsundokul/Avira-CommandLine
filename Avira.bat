222f10dd1483a4bd1abbb97f176437a603f77882
Segovia <35817798+SegoCode@users.noreply.github.com>
2018-04-19 14:37:52 +0200

    UAC

---
+++ Avira.bat
@@ -0,0 +1,22 @@
+@echo off
+TITLE Avira command line scanner (All in One)
+:: ---------------------ADMIN PRIVILEGES-------------------
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+::---------------------------------------------------------
